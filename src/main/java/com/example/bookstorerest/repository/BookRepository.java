5260f2fca9cec3c4d9a49c66e81d42d8e270a4a1
Iakamenyuk <skritex0@gmail.com>
2025-03-06 20:01:24 +0300

    add

--- src/main/java/com/example/bookstorerest/repository/BookRepository.java
+++ src/main/java/com/example/bookstorerest/repository/BookRepository.java
@@ -3,5 +3,13 @@ package com.example.bookstorerest.repository;
 import com.example.bookstorerest.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByNameBook(String nameBook);
+
+    Optional<Book> findByNameAuthor(String nameAuthor);
+
+    Optional<Book> findByGenre(String genre);
 }
--- src/main/java/com/example/bookstorerest/service/BookService.java
+++ src/main/java/com/example/bookstorerest/service/BookService.java
@@ -1,11 +1,15 @@
 package com.example.bookstorerest.service;
 
 import com.example.bookstorerest.entity.Book;
+import com.example.bookstorerest.entity.User;
 import com.example.bookstorerest.repository.BookRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Optional;
 
 @Slf4j
@@ -24,6 +28,51 @@ public class BookService {
         } else {
             log.warn("User with id '" + id + "' not found");
             return Optional.empty();
+        }
+    }
+
+    public Optional<Book> findByNameBook(String nameBook){
+        Optional<Book> optionalBook = bookRepository.findByNameBook(nameBook);
+        if(optionalBook.isPresent()){
+            log.info("Book with bookName '" + nameBook + "' was found");
+            return optionalBook;
+        }else {
+            log.error("Book with bookName '" + nameBook + "' wasn't found");
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Book> findByNameAuthor(String nameAuthor){
+        Optional<Book> optionalBook = bookRepository.findByNameAuthor(nameAuthor);
+        if(optionalBook.isPresent()){
+            log.info("Book with nameAuthor '" + nameAuthor + "' was found");
+            return optionalBook;
+        }else {
+            log.error("Book with nameAuthor '" + nameAuthor + "' wasn't found");
+            return Optional.empty();
+        }
+    }
+    public Optional<Book> findByGenre(String genre){
+        Optional<Book> optionalBook = bookRepository.findByGenre(genre);
+        if(optionalBook.isPresent()){
+            log.info("Book with genre '" + genre + "' wasn't found");
+            return optionalBook;
+        }else {
+            log.error("Book with genre '" + genre + "' wasn't found");
+            return Optional.empty();
+        }
+    }
+    public BufferedReader createBufferReader(InputStreamReader inputStreamReader){
+        return new BufferedReader(inputStreamReader);}
+
+    public void readAndSaveDescription(){
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("bookDescription.txt");
+        if(inputStream == null){
+            log.error("file not found");
+            return;
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        try (BufferedReader bufferedReader = createBufferReader(inputStreamReader)){
         }
     }
 }
